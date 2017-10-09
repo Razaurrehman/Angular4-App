0bb688f5ca9466b969df341a8d79d1983de5cc57
RazaUrRehman <razaurrehman1991@hotmail.com>
2017-10-10 04:37:08 +0500

    UI learning

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // veiwMode = 'map';
+  // courses = [
+  //   {id : 1 , name : 'course1'},
+  //   {id : 2 , name : 'course2'},
+  //   {id : 3 , name : 'course3'},
+  // ]
+  // onadd(){
+  //   this.courses.push({
+  //     id:4,
+  //     name: 'course4'
+  //   })
+  // };
+  // onRemove(course){
+  //   let indexvalue = this.courses.indexOf(course);
+  //   this.courses.splice(indexvalue,1);
+  // }
+
+  // email="abx@gmail.com"
+  // keyup(){
+  //   console.log(this.email);
+  // }
+
+  
+
+
+}
